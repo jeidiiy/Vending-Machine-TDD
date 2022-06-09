7e0d1a928e14efac704b9bb19062d9ed9b2d42c0
jeidiiy <wldyd2356@gmail.com>
2022-06-09 22:21:21 +0900

    refactor: 불필요한 중복 테스트 제거

--- src/test/java/AppTests.java
+++ src/test/java/AppTests.java
@@ -8,32 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTests {
 
     @Test
-    void testGetWater() {
+    void testGetBeverage() {
         VendingMachine vendingMachine = new VendingMachine();
         vendingMachine.stockUp(WATER);
         vendingMachine.inputMoney(1000);
         Beverage water = vendingMachine.selectBeverage(WATER);
         assertEquals(water.getName(), "water");
-        assertEquals(0, vendingMachine.getBalance());
-    }
-
-    @Test
-    void testGetCoke() {
-        VendingMachine vendingMachine = new VendingMachine();
-        vendingMachine.stockUp(COKE);
-        vendingMachine.inputMoney(1200);
-        Beverage coke = vendingMachine.selectBeverage(COKE);
-        assertEquals(coke.getName(), "coke");
-        assertEquals(0, vendingMachine.getBalance());
-    }
-
-    @Test
-    void testGetSprite() {
-        VendingMachine vendingMachine = new VendingMachine();
-        vendingMachine.stockUp(SPRITE);
-        vendingMachine.inputMoney(1100);
-        Beverage sprite = vendingMachine.selectBeverage(SPRITE);
-        assertEquals(sprite.getName(), "sprite");
         assertEquals(0, vendingMachine.getBalance());
     }
 }
